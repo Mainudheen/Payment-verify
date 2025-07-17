b224b4f24516e9e3433640cba9065661ffe486b2
Mainudheen <mainudheens.23aid@kongu.edu>
2025-07-17 21:28:50 +0530

    modify index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/students',require('./routes/students'));
 app.use('/api/payments',require('./routes/payments'));
-
+//connected
 //connect MongoDb
 
 mongoose
